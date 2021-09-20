d69087617a0d91ce78459013e36a0602db74ff62
st094705Artem <st094705@student.spbu.ru>
2021-09-20 16:11:01 +0300

    Homework code

--- Homework18.09/Task1-10/Task10/Source.cpp
+++ Homework18.09/Task1-10/Task10/Source.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-
+	int a,b;
+	cin >> a>>b;
+	int k = a / b;
+	int l = b / a;
+	int c = k + l;
+	cout << (a*k + b * l) / c;
+	return EXIT_SUCCESS;
 }
